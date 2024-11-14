8a1f671cbaccd8417942a88401f3a491475a537f
Jim Chen <jimchen4214@gmail.com>
2024-11-15 05:19:07 +0800

    Updates

---
+++ README.md
@@ -0,0 +1,19 @@
+## Install
+
+```
+cd ~/Code
+git clone https://github.com/jimchen2/local-video-shortcut-extension
+
+
+mkdir -p ~/.config/google-chrome/Custom\ Extensions/
+cp -r ~/Code/local-video-shortcut-extension ~/.config/google-chrome/Custom\ Extensions/
+```
+
+## Update
+
+```
+rm -rf ~/.config/google-chrome/Custom\ Extensions/local-video-shortcut-extension
+cp -r ~/Code/local-video-shortcut-extension ~/.config/google-chrome/Custom\ Extensions/
+```
+
+Go to chrome://extensions/ load unpacked
--- content.js
+++ content.js
@@ -2,31 +2,36 @@ document.addEventListener('keydown', function(event) {
     const video = document.querySelector('video');
     if (!video) return;
 
-    switch(event.key) {
+    switch(event.key.toLowerCase()) {
         case 'f':
+        case 'а': // Russian 'f'
             event.preventDefault();
             document.fullscreenElement ? document.exitFullscreen() : video.requestFullscreen();
             break;
         case '<':
+        case 'б': // Russian 'b'
             event.preventDefault();
             video.playbackRate = Math.max(0.25, video.playbackRate - 0.25);
             break;
         case '>':
+        case 'ю': // Russian 'yu'
             event.preventDefault();
             video.playbackRate = Math.min(2, video.playbackRate + 0.25);
             break;
         case 'j':
+        case 'о': // Russian 'o'
             event.preventDefault();
             video.currentTime -= 10;
             break;
         case 'k':
+        case 'л': // Russian 'l'
             event.preventDefault();
             video.paused ? video.play() : video.pause();
             break;
         case 'l':
+        case 'д': // Russian 'd'
             event.preventDefault();
             video.currentTime += 10;
             break;
     }
 });
-
